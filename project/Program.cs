467395d0b629641cae69e72c6cc89c16fe730dc1
CWX <91418059+CWXDEV@users.noreply.github.com>
2022-03-09 21:33:12 +0000

    initial commit
    initial commit

---
+++ project/Program.cs
@@ -0,0 +1,16 @@
+﻿using Aki.Common.Utils;
+using BepInEx;
+
+namespace DeSharpener
+{
+    [BepInPlugin("com.CWX.DeSharpener", "CWX-DeSharpener", "2.0.0")]
+    public class Program : BaseUnityPlugin
+    {
+        private void Main()
+        {
+            Logger.LogMessage("Loading: CWX DESHARPENER");
+
+            new DeSharpener().Enable();
+        }
+    }
+}
