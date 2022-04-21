40a9224cd639a3b00d37e5da09f89917c231b957
CWX <CWXDEV@outlook.com>
2022-04-21 03:21:40 +0100

    added shared folder for easy solution, added config to plugin, need to work out work to apply value properly

--- project/Program.cs
+++ project/Program.cs
@@ -1,12 +1,23 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 
 namespace DeSharpener
 {
-    [BepInPlugin("com.CWX.DeSharpener", "CWX-DeSharpener", "2.0.0")]
+    [BepInPlugin("com.CWX.DeSharpener", "CWX-DeSharpener", "1.3.0")]
     public class Plugin : BaseUnityPlugin
     {
+        public static ConfigEntry<float> sharpness;
+
         private void Awake()
         {
+            sharpness = Config.Bind
+                (
+                "Sharpness Value",
+                "Sharpness",
+                0.7f,
+                "Set the sharpness value for your game to run at, " +
+                "this will not change when using painkillers"
+                );
             new DeSharpener().Enable();
         }
     }
